f4640dabc9c38897d0f8378e50dcc8c0406a00b1
MrBageur <124700771+MrBageur@users.noreply.github.com>
2025-01-02 00:40:21 +0700

    Replace all files with the latest version

--- resources/views/profile.blade.php
+++ resources/views/profile.blade.php
@@ -31,48 +31,51 @@
                 </ul>
             </div>
         </div>
-        <div class="container">
-        <div class="profile-container">
-            <h2>Profile Saya</h2>
-            <div class="profile-info">
-                <div class="profile-item">
-                    <strong>Nama:</strong> {{ Auth::user()->name }}
+        <div class="profile-wrapper">
+            <div class="profile-container">
+                <h2>Profile Saya</h2>
+                <div class="profile-info">
+                    <div class="picture-akun">
+                        <img src="../images/GambarAkun.png" alt="GambarAkun">
+                    </div>
+                    <div class="profile-item">
+                        <strong>Nama:</strong> {{ Auth::user()->name }}
+                    </div>
+                    <div class="profile-item">
+                        <strong>Email:</strong> {{ Auth::user()->email }}
+                    </div>
+                    <div class="profile-item">
+                        <strong>Jenis Pendaftar:</strong> {{ Auth::user()->jenis_pendaftar }}
+                    </div>
+                    <div class="profile-item">
+                        <strong>Asal Sekolah:</strong> {{ Auth::user()->asal_sekolah }}
+                    </div>
+                    <div class="profile-item">
+                        <strong>Jenjang yang Ditujui:</strong> {{ Auth::user()->jenjang }}
+                    </div>
+                    <div class="profile-item">
+                        <strong>NIK:</strong> {{ Auth::user()->nik }}
+                    </div>
+                    <div class="profile-item">
+                        <strong>NISN:</strong> {{ Auth::user()->nisn }}
+                    </div>
+                    <div class="profile-item">
+                        <strong>Tempat Lahir:</strong> {{ Auth::user()->tempat_lahir }}
+                    </div>
+                    <div class="profile-item">
+                        <strong>Tanggal Lahir:</strong> {{ Auth::user()->tanggal_lahir }}
+                    </div>
+                    <div class="profile-item">
+                        <strong>Jenis Kelamin:</strong> {{ Auth::user()->jenis_kelamin }}
+                    </div>
                 </div>
-                <div class="profile-item">
-                    <strong>Email:</strong> {{ Auth::user()->email }}
-                </div>
-                <div class="profile-item">
-                    <strong>Jenis Pendaftar:</strong> {{ Auth::user()->jenis_pendaftar }}
-                </div>
-                <div class="profile-item">
-                    <strong>Asal Sekolah:</strong> {{ Auth::user()->asal_sekolah }}
-                </div>
-                <div class="profile-item">
-                    <strong>Jenjang yang Ditujui:</strong> {{ Auth::user()->jenjang }}
-                </div>
-                <div class="profile-item">
-                    <strong>NIK:</strong> {{ Auth::user()->nik }}
-                </div>
-                <div class="profile-item">
-                    <strong>NISN:</strong> {{ Auth::user()->nisn }}
-                </div>
-                <div class="profile-item">
-                    <strong>Tempat Lahir:</strong> {{ Auth::user()->tempat_lahir }}
-                </div>
-                <div class="profile-item">
-                    <strong>Tanggal Lahir:</strong> {{ Auth::user()->tanggal_lahir }}
-                </div>
-                <div class="profile-item">
-                    <strong>Jenis Kelamin:</strong> {{ Auth::user()->jenis_kelamin }}
-                </div>
+                
+                <form method="POST" action="{{ route('logout') }}">
+                    @csrf
+                    <button type="submit" class="button">Logout</button>
+                </form>
             </div>
-            
-            <form method="POST" action="{{ route('logout') }}">
-                @csrf
-                <button type="submit" class="button">Logout</button>
-            </form>
         </div>
-    </div>
     </div>
 </body>
 
